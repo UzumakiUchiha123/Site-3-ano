6f4bb6b432f4ae23a32045e3cb10462433bcb795
Kevellyn.lima <110827364+UzumakiUchiha123@users.noreply.github.com>
2024-03-20 12:20:15 -0300

    5

--- main.js
+++ main.js
@@ -3,8 +3,18 @@ console.log(botões);
 for (let i=0;i <botoes.length;i++) {
     botoes[i].onclick=function(){
         for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-        }
-      botoes[i].classList.add("ativo");
-    }
+            botoes[j].classList.remove("ativo"); }
+      botoes[i].classList.add("ativo");  }
 }
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+for (let i = 0; i < botoes.length; i++) {
+     botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
+            botoe[j].classList.remove("ativo");
+            textos[j].classList.remove("aivo"); }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo"); }}
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo
+contadores[0].textContent = "Contagem regresiva";
